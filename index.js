63102db4076b1888b85b5b7dc56b7024b754df63
KaoutharDraif <kaouthardraif@gmail.com>
2024-12-11 02:56:14 +0100

    JS Update

---
+++ index.js
@@ -0,0 +1,56 @@
+// First Project PopUp
+function PopupYummyApp() {
+    document.getElementById(
+        "PopupYummyApp"
+    ).style.display = "block";
+}
+function closePopupYummyApp() {
+    document.getElementById(
+        "PopupYummyApp"
+    ).style.display = "none";
+}
+// Second Project PopUp
+function PopupTodolist() {
+    document.getElementById(
+        "PopupTodolist"
+    ).style.display = "block";
+}
+function closePopupTodolist() {
+    document.getElementById(
+        "PopupTodolist"
+    ).style.display = "none";
+}
+// Third Project PopUp
+function PopupPersoPortfolio() {
+    document.getElementById(
+        "PopupPersoPortfolio"
+    ).style.display = "block";
+}
+function closePopupPersoPortfolio() {
+    document.getElementById(
+        "PopupPersoPortfolio"
+    ).style.display = "none";
+}
+// Fourth Project PopUp
+function PopupNoteapp() {
+    document.getElementById(
+        "PopupNoteapp"
+    ).style.display = "block";
+}
+function closePopupNoteapp() {
+    document.getElementById(
+        "PopupNoteapp"
+    ).style.display = "none";
+}
+// Fifth Project PopUp
+
+function PopupWeatherTracker() {
+    document.getElementById(
+        "PopupWeatherTracker"
+    ).style.display = "block";
+}
+function closePopupWeatherTracker() {
+    document.getElementById(
+        "PopupWeatherTracker"
+    ).style.display = "none";
+}
